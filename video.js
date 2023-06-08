484d8f9388cf95ed7cfc968905a766a3b244775d
Zau Seng <110077742+Mnaw161329@users.noreply.github.com>
2023-06-08 21:04:46 +0630

    Update video.js

--- video.js
+++ video.js
@@ -3,21 +3,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (parentElement) {
         const srcList = [
-            " ./video/1.mp4 ",
-            " ./video/2.mp4 ",
-            " ./video/3.mp4 ",
-            " ./video/4.mp4 ",
             " https://youtube.com/embed/o1HCcCYKjdk ",
-            "  ",
-            " ./video/5.mp4 ",
-            " ./video/6.mp4 ",
-            " ./video/7.mp4 ",
-            " ./video/8.mp4 ",
-            " ./video/9.mp4 ",
-            "  ",
-            "  ",
-            "  ",
-            "  ",
+ 
+            " ./img/1.mp4 ",
+            " ./img/2.mp4 ",
+            " ./img/1.mp4 ",
+            " ./img/2.mp4 ",
+
+
+
+
+
         ];
 
         for (let i = 0; i < srcList.length; i++) {
@@ -36,17 +32,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (videos) {
         const srcListVideo = [
-            " ./video/a.mp4 ",
+            " ./img/1.mp4 ",
             "  ", 
-            " ./video/b.mp4 ",
-            " ./video/c.mp4 ",
+            " ./img/2.mp4 ",
+            " ./img/1.mp4 ",
             "  ",
             " ./video/d.mp4 ",
             " ./video/e.mp4 ",
             "  ",
-            " ./video/f.mp4 ",
-            "  ",
-            "  ",
+            " ./img/2.mp4 ",
+
+
         ];
 
         for (let i = 0; i < srcListVideo.length; i++) {
